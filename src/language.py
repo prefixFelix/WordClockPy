de624b0140cf5adcb1a42799b2422b2282f8f0ee
prefix <f.social@mailbox.org>
2025-07-20 20:05:12 +0200

    feat: update README

--- src/language.py
+++ src/language.py
@@ -1,4 +1,4 @@
-# Language presets. Should not been edited.
+# Language presets.
 
 lang_german = {
     # 0 column  10
@@ -36,3 +36,28 @@ lang_german = {
     'hour_12': (8, (6, 7, 8, 9, 10)),
     'o_clock': (9, (8, 9, 10)),
 }
+
+# --- Language dict explanation ---
+#
+#  Create a new dict with the name of your language and copy all the existing KEYS into it:
+#      lang_your_new_language = {...}
+#  A key represents a word and the value (nested tuples) represent the coordinates of the letters that make up the individual word.
+#  The coordinates always refer to the front plate and are fixed (do not change depending on the LED layout).
+#  The exact indices of the LEDs are later automatically derived from these coordinates depending on the layout.
+#
+#  This represents the coordinates of a german front plate. X:0; Y:0 is always top left.
+#  0 column  10
+#  ESKISTAFÜNF 0
+#  ZEHNZWANZIG
+#  DREIVIERTEL
+#  VORFUNKNACH r
+#  HALBAELFÜNF o
+#  EINSXAMZWEI w
+#  DREIPMJVIER
+#  SECHSNLACHT
+#  SIEBENZWÖLF
+#  ZEHNEUNKUHR 9
+#  All the letters in the word 'ES IST' are positioned at Y-coordinate of 0. 'ES' is be positioned at x-coordinates 0 and 1. 'IST' is at x-coordinates 3, 4, and 5. So the key / value pair looks like this:
+#           Y   X  X  X  X  X
+# 'it_is': (0, (0, 1, 3, 4, 5)),
+
